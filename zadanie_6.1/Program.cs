bc6e8e2acd26498ef30cb5b198dab5a2c39beab7
Vlad Dre <dreier.vlad@mail.ru>
2023-12-14 18:51:48 +0300

    Задание 6.1

---
+++ zadanie_6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void NumberLine(int m, int n){
+    if(m == n)  return;
+    Console.WriteLine($"{m} ","");
+    NumberLine(m+1 , n);
+}
+
+
+
+
+Console.WriteLine("Enter your number");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your number");
+int n = Convert.ToInt32(Console.ReadLine());
+NumberLine(m,n);
