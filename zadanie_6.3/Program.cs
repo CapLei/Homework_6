ba57ec3d450b965d1b56d60fe7e23b5684d9fba6
Vlad Dre <dreier.vlad@mail.ru>
2023-12-14 19:12:34 +0300

    Задание 6.3

---
+++ zadanie_6.3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int[] array = {1,2,3,4,5};
+int i = array.Length-1;
+
+
+void ArrayShow(int [] array, int i){
+    if(i<0) return;
+    int result = array[i];
+    Console.WriteLine($"{result}");
+
+    ArrayShow(array,i-1);
+}
+
+ArrayShow(array,i);
