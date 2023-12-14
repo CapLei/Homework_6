a8c7d1c86165b40f1c52c50380d3dbd14f714d99
Vlad Dre <dreier.vlad@mail.ru>
2023-12-14 19:12:23 +0300

    Задание 6.2

---
+++ zadanie_6.2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int Akkerman(int m,int n){
+    if(m == 0) return n+1;
+    else if(n == 0) return Akkerman(m-1, 1);
+    else return Akkerman(m-1, Akkerman(m, n-1));
+}
+
+
+Console.WriteLine("Enter number m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int akkerman = Akkerman(m,n);
+Console.WriteLine($"Функция Аккермана равна {akkerman}");
